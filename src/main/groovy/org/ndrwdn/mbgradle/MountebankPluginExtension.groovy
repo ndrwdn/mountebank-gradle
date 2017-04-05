ef97cff3df9251598ce9aad476e7f43f305974c5
Austin Garrard <agarrard@thoughtworks.com>
2017-04-05 11:31:09 -0500

    configure all mb cli arguments

--- src/main/groovy/org/ndrwdn/mbgradle/MountebankPluginExtension.groovy
+++ src/main/groovy/org/ndrwdn/mbgradle/MountebankPluginExtension.groovy
@@ -4,5 +4,18 @@ class MountebankPluginExtension {
     String extractPath = 'mb'
     long startTimeout = 5000
     long stopTimeout = 5000
-    boolean allowInjection = false
+    Integer port = null
+    String configFile = null
+    Boolean noParse = null
+    String logFile = null
+    String logLevel = null
+    Boolean noLogFile = null
+    Boolean allowInjection = null
+    Boolean localOnly = null
+    String ipWhitelist = null
+    Boolean mock = null
+    Boolean debug = null
+    String pidFile = null
+    String saveFile = null
+    Boolean removeProxies = null
 }
